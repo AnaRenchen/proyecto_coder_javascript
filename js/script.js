273fb2533b79fbe713e414cc083e2f6e97015059
AnaRenchen <anamagbh@gmail.com>
2023-11-02 20:01:40 -0300

    agregue comentarios

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-  
+  // Clase para los productos
+
   class Producto {
 
   static id = 0;
@@ -12,7 +13,7 @@
     this.subtotal = 0;
   }
  infoProducto () {
-  console.log ("Nombre: " + this.nombre + "\nPrecio: " + this.precio + "\nCantidad: " + this.cantidad + "\nVendido: " + this.vendido);
+  console.log ("Nombre: " + this.nombre + "\nPrecio: " + this.precio + "\nCantidad: " + this.cantidad + "\nVendido: " + this.vendido + "\n\n");
  }
 
  calcularSubtotal () {
@@ -21,17 +22,24 @@
 
 }
 
+// Instanciar productos
+
 const pintura1 = new Producto ("Karajishi", 250, 1);
 const pintura2 = new Producto ("Samurai", 250, 1);
 const pintura3 = new Producto ("Koi", 250, 1);
+
+
+// Mostrar productos en consola
 
 pintura1.infoProducto();
 pintura2.infoProducto();
 pintura3.infoProducto();
 
+// Crear un array con el stock de productos y un array de carrito de compras
 const stockProductos = [pintura1, pintura2, pintura3];
 let carrito = [];
 
+// Funcion para mostrar los productos y precios para que el usuario pueda elegir
 
 function mostrarProductos() {
   let mostrarProductos = "";
@@ -42,6 +50,7 @@ function mostrarProductos() {
   console.log("Nuestra lista de Productos:\n" + mostrarProductos);
 }
 
+// Funcion para que el usuario pueda elegir los productos a partir de la lista
 
 function elegirProducto() {
   mostrarProductos();
@@ -51,7 +60,8 @@ function elegirProducto() {
     alert("La compra ha sido cancelada.");
     return;
   }
-      
+      // Verificar si el producto elegido esta en el array de stock y si está preguntar por la cantidad
+
       productoElegido = stockProductos.find(producto => producto.nombre.toLowerCase() === productoElegido.toLowerCase());
 
       if (productoElegido) {
@@ -65,105 +75,13 @@ function elegirProducto() {
     }
       elegirProducto();
 
-          /*if (cantidadCompra <= productoElegido.cantidad) {
-            productoElegido.cantidad -= cantidadCompra;
-            productoElegido.calcularSubtotal();
-
-
-          let productoEncarrito = carrito.find(item => item.nombre === productoElegido.nombre);
-
-          if (productoEncarrito) {
-            productoEncarrito.cantidad += cantidadCompra;
-            productoEncarrito.calcularSubtotal();
-          } else {
-            productoEncarrito = new Producto(productoElegido.nombre, productoElegido.precio, cantidadCompra);
-            carrito.push(productoEncarrito);
-          }
-    
-          alert("Producto agregado al carrito.");
-        } else {
-          alert("No hay suficiente stock para esa cantidad.");
-        }
-      } else {
-        alert("Producto no válido, por favor ingrese un producto de la lista.");
-      }
-    }
-
-    elegirProducto();
-    
-    
+          
     
       
 
         
     
     
-      /*function agregarProducto (productoElegido) {
-        if (carrito.includes(productoElegido)) {
-          alert("Error al agregar el producto " + productoElegido.nombre + " porque ya está en su carrito.");
-          console.log(carrito);
-        } else {
-          carrito.push(producto.productoElegido);
-          alert("Se agrego el producto " + productoElegido.nombre + " al carrito.");
-          console.log(carrito);
-        }
-      }
+
   
-    function agregarNuevoProducto (){
-      const producto = elegirProducto();
-      if (productoElegido === null) {
-        alert ("La compra ha sido cancelada.");
-      } else{
-      agregarProducto(producto);
-  
-      let confirmar = confirm('¿Desea agregar un nuevo producto?');
-      if (confirmar){
-        agregarNuevoProducto();
-      } else{
-        alert ("Finalizaremos la compra y en seguida usted será dirigido al sistema de envío y pago.");
-      }
-    }
-  }
-  
-  agregarNuevoProducto();
-  
-
-/*let producto;
-let productoSeleccionado;
-
-
-const productos = ["pintura1", "pintura2", "pintura3", "pintura4", "pintura5", "pintura6", "pintura7", "pintura8", "pintura9", "pintura10"];
-
-let listaProductos = "";
-
-for (let i=0; i < productos.length; i++){
-listaProductos+= productos[i] + "\n";
-}
-
-alert("Nuestra lista de Productos:\n" + listaProductos);
-console.log ("Nuestra lista de Productos:\n" + listaProductos);
-
-const carritoCompras =[];
-
-console.log (carritoCompras);
-
-
-
-let resultado;
-let cantidadProductos = carritoCompras.length;
-const precio = 200;
-const envio = (50 * cantidadProductos);
-
-const precioTotal = (cantidadProductos, precio, envio) => (cantidadProductos * precio) + envio;
-
-function mostrarPrecioTotal (mostrarTotal){
-  alert ("El total de su compra con envío es: " + mostrarTotal);
-  console.log ("El total de su compra con envío es: " + mostrarTotal);
-}
-
-resultado = precioTotal (cantidadProductos, precio, envio);
-
-if (carritoCompras.length > 0){
-mostrarPrecioTotal(resultado);
-}*/
 
