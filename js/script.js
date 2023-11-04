2b13e9081b359a9e3d8f39637136a290c0c4c836
AnaRenchen <anamagbh@gmail.com>
2023-11-04 19:41:18 -0300

    cambios codigo js para carrito de compras

--- js/script.js
+++ js/script.js
@@ -24,9 +24,9 @@
 
 // Instanciar productos
 
-const pintura1 = new Producto ("Karajishi", 250, 1);
-const pintura2 = new Producto ("Samurai", 250, 1);
-const pintura3 = new Producto ("Koi", 250, 1);
+const pintura1 = new Producto ("karajishi", 250, 1);
+const pintura2 = new Producto ("samurai", 250, 1);
+const pintura3 = new Producto ("koi", 250, 1);
 
 
 // Mostrar productos en consola
@@ -39,41 +39,91 @@ pintura3.infoProducto();
 const stockProductos = [pintura1, pintura2, pintura3];
 let carrito = [];
 
+
+
 // Funcion para mostrar los productos y precios para que el usuario pueda elegir
 
 function mostrarProductos() {
   let mostrarProductos = "";
-  for (let i = 0; i < stockProductos.length; i++) {
-    mostrarProductos += ("Nombre: " + stockProductos[i].nombre + "\nPrecio: " + stockProductos[i].precio + "\n\n");
-  }
+
+  stockProductos.forEach ((producto) => {
+    mostrarProductos += ("Nombre: " + producto.nombre + "\nPrecio: " + producto.precio + "\n\n")});
   alert("Nuestra lista de Productos:\n" + mostrarProductos);
   console.log("Nuestra lista de Productos:\n" + mostrarProductos);
 }
 
-// Funcion para que el usuario pueda elegir los productos a partir de la lista
+// Funcion para que el usuario pueda elegir los productos a partir de la lista, la cantidad deseada y agregar al carrito
 
 function elegirProducto() {
+
   mostrarProductos();
+
+  while (true){
+
+  let productoExiste;
   let productoElegido = prompt("Por favor escriba el nombre de la pintura que le gustaría comprar.");
 
   if (productoElegido === null) {
     alert("La compra ha sido cancelada.");
-    return;
-  }
-      // Verificar si el producto elegido esta en el array de stock y si está preguntar por la cantidad
+    break;
+  } else{
+      // Verificar si el producto elegido esta en el array de stock 
 
-      productoElegido = stockProductos.find(producto => producto.nombre.toLowerCase() === productoElegido.toLowerCase());
+      productoExiste = stockProductos.find(producto => producto.nombre.toLowerCase() === productoElegido.toLowerCase())
 
-      if (productoElegido) {
-        let cantidadCompra = parseInt(prompt ("Por favor ingrese la cantidad de productos que desea comprar."));
+     if (productoExiste){
 
-        if (isNaN(cantidadCompra) || cantidadCompra === null){
-          alert ("Por favor ingrese un número válido.");
-          return;
-        }
-      }
+      let cantidadCompra = parseInt (prompt("¿Qué cantidad de producto desea comprar?"));
+
+      // Asegurarse de que la cantidad sea válida y que se actualice el stock y el subtotal
+
+      if (isNaN(cantidadCompra) || cantidadCompra === ""){
+        alert ("Por favor ingrese una cantidad válida");
+      } else if (cantidadCompra <= productoExiste.cantidad){
+        productoExiste.cantidad -= cantidadCompra;
+        productoExiste.calcularSubtotal();
+        carrito.push(productoExiste);
+    alert("Se agregó el producto " + productoExiste.nombre + " al carrito.");
+
+    const continuar = prompt ("¿Desea seguir agregando producto? Por favor escriba si o no y haga click en aceptar."). toLowerCase();
+    if (continuar === "no"){
+      break;
     }
-      elegirProducto();
+      } else {
+        alert ("No hay cantidad suficiente del producto en stock.")
+      }
+    } else {
+      alert ("Por favor ingrese un producto de la lista.")
+    }
+  }
+}
+mostrarCarrito();
+}
+
+// Funcion para mostrar el carrito de compras con el total de la compra
+
+function mostrarCarrito () {
+   
+  let mensajeCarrito = "Este es el detalle de su carrito de compras:\n";
+  
+   carrito.forEach ((producto) => {mensajeCarrito+= ("- Producto: " + producto.nombre + "\n- Cantidad: " + producto.cantidad + "\n- Subtotal: " + producto.subtotal + "\n\n")});
+  
+   const total = carrito.reduce ((suma, producto) => suma + producto.subtotal, 0);
+   mensajeCarrito += "- Total: " + total + "\n ¡Excelente elección! Ahora usted será dirigido al sistema de envío y pago.";
+  
+   alert (mensajeCarrito);
+  } 
+    elegirProducto();
+    
+
+   
+
+      
+
+    
+
+
+  
 
           
     
