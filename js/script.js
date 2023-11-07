0206e0871c0d1dfc4bb8527adb7b8a45e88de982
AnaRenchen <anamagbh@gmail.com>
2023-11-07 12:00:52 -0300

    cambios js y html

--- js/script.js
+++ js/script.js
@@ -1,84 +1,96 @@
-  // Clase para los productos
+  // Clase para los Productos en general
 
-  class Producto {
+  class Productos {
 
   static id = 0;
 
   constructor (nombre, precio, cantidad, vendido, subtotal) {
-    this.id = ++Producto.id;
+    this.id = ++Productos.id;
     this.nombre = nombre;
     this.precio = precio;
     this.cantidad = cantidad;
     this.vendido = false;
     this.subtotal = 0;
   }
- infoProducto () {
+ infoProductos () {
   console.log ("Nombre: " + this.nombre + "\nPrecio: " + this.precio + "\nCantidad: " + this.cantidad + "\nVendido: " + this.vendido + "\n\n");
  }
 
  calcularSubtotal () {
-  this.total = (this.precio * this.cantidad);
+  this.stotaltotal = (this.precio * this.cantidad);
  }
 
 }
 
+// Clase para representar remeras
+class Remera extends Productos {
+  constructor(nombre, precio, cantidad, vendido, subtotal, talles) {
+    super(nombre, precio, cantidad, vendido, subtotal);
+    this.talles = talles;
+  }
+}
+
 // Instanciar productos
 
-const pintura1 = new Producto ("kiku", 200, 1);
-const pintura2 = new Producto ("dakki no ohyaku", 380, 1);
-const pintura3 = new Producto ("mongaku shonin", 108, 1);
-const pintura4 = new Producto ("kiyo hime", 450, 1);
-const pintura5 = new Producto ("unryu", 280, 1);
-const pintura6 = new Producto ("hou-oo", 350, 1);
-const pintura7 = new Producto ("kasha", 190, 1);
-const pintura8 = new Producto ("nio guardians ", 299, 1);
-const pintura9 = new Producto ("jiraya", 300, 1);
-const pintura10 = new Producto ("karajishi", 230, 1);
-const pintura11 = new Producto ("kingyo", 290, 1);
-const pintura12 = new Producto ("yamauba to kintaro", 400, 1);
-const pintura13 = new Producto ("kintaro to koi", 450, 1);
-const pintura14 = new Producto ("kitsune", 250, 1);
-const pintura15 = new Producto ("kiyohime", 380, 1);
-const pintura16 = new Producto ("orochimaru", 300, 1);
-const pintura17 = new Producto ("tsuru", 200, 1);
-const pintura18 = new Producto ("botan", 250, 1);
-const pintura19 = new Producto ("danshichi kurobei", 500, 1);
-const pintura20 = new Producto ("suimon yaburi", 450, 1);
-const pintura21 = new Producto ("oniwakamaru ", 500, 1);
-const pintura22 = new Producto ("tsunade", 300, 1);
-const pintura23 = new Producto ("tennin", 300, 1);
-const pintura24 = new Producto ("tora", 300, 1);
-const pintura25 = new Producto ("flash", 270, 1);
+const pintura1 = new Productos ("kiku", 200, 1);
+const pintura2 = new Productos ("dakki no ohyaku", 380, 1);
+const pintura3 = new Productos ("mongaku shonin", 108, 1);
+const pintura4 = new Productos ("kiyo hime", 450, 1);
+const pintura5 = new Productos ("unryu", 280, 1);
+const pintura6 = new Productos ("hou-oo", 350, 1);
+const pintura7 = new Productos ("kasha", 190, 1);
+const pintura8 = new Productos ("nio guardians ", 299, 1);
+const pintura9 = new Productos ("jiraya", 300, 1);
+const pintura10 = new Productos ("karajishi", 230, 1);
+const pintura11 = new Productos ("kingyo", 290, 1);
+const pintura12 = new Productos ("yamauba to kintaro", 400, 1);
+const pintura13 = new Productos ("kintaro to koi", 450, 1);
+const pintura14 = new Productos ("kitsune", 250, 1);
+const pintura15 = new Productos ("kiyohime", 380, 1);
+const pintura16 = new Productos ("orochimaru", 300, 1);
+const pintura17 = new Productos ("tsuru", 200, 1);
+const pintura18 = new Productos ("botan", 250, 1);
+const pintura19 = new Productos ("danshichi kurobei", 500, 1);
+const pintura20 = new Productos ("suimon yaburi", 450, 1);
+const pintura21 = new Productos ("oniwakamaru ", 500, 1);
+const pintura22 = new Productos ("tsunade", 300, 1);
+const pintura23 = new Productos ("tennin", 300, 1);
+const pintura24 = new Productos ("tora", 300, 1);
+const pintura25 = new Productos ("flash", 270, 1);
+const incienso = new Productos ("incienso", 2, 10);
+const mate = new Productos ("mate", 10, 10);
+const pulsera = new Productos ("pulsera", 10, 10);
+const remera_fudoshin = new Remera ("remera fudoshin", 20, 10, ["s","m", "l"]);
+const remera_trai = new Remera ("remera trailokyavijaya", 20, 10, ["s","m", "l"])
 
 
 
-// Crear un array con el stock de productos y un array de carrito de compras
-const stockProductos = [pintura1, pintura2, pintura3, pintura4, pintura5, pintura6, pintura7, pintura8, pintura9, pintura10, pintura11, pintura12, pintura13, pintura14, pintura15, pintura16, pintura17, pintura18,pintura19, pintura20, pintura21, pintura22, pintura23, pintura24, pintura25];
+// Crear un array con el stock de Productoss y un array de carrito de compras
+const stockProductos = [pintura1, pintura2, pintura3, pintura4, pintura5, pintura6, pintura7, pintura8, pintura9, pintura10, pintura11, pintura12, pintura13, pintura14, pintura15, pintura16, pintura17, pintura18,pintura19, pintura20, pintura21, pintura22, pintura23, pintura24, pintura25, incienso, mate, pulsera, remera_fudoshin, remera_trai];
 let carrito = [];
 
 
-
-// Funcion para mostrar los productos y precios para que el usuario pueda elegir
+// Funcion para mostrar los Productoss y precios para que el usuario pueda elegir
 
 function mostrarProductos() {
   let mostrarProductos = "";
 
-  stockProductos.forEach ((producto) => {
-    mostrarProductos += ("Nombre: " + producto.nombre + "\nPrecio: " + producto.precio + "\n\n")});
-  alert("Nuestra lista de Productos:\n" + mostrarProductos);
-  console.log("Nuestra lista de Productos:\n" + mostrarProductos);
+  stockProductos.forEach ((productos) => {
+    mostrarProductos += ("Nombre: " + productos.nombre + "\nPrecio: " + productos.precio + "\n\n")});
+  alert("Nuestra lista de Productoss:\n" + mostrarProductos);
+  console.log("Nuestra lista de Productoss:\n" + mostrarProductos);
 }
 
-// Funcion para que el usuario pueda elegir los productos a partir de la lista, la cantidad deseada y agregar al carrito
+// Funcion para que el usuario pueda elegir los Productoss a partir de la lista, la cantidad deseada y agregar al carrito
 
-function elegirProducto() {
+function elegirProductos() {
 
   mostrarProductos();
 
   while (true){
 
   let productoExiste;
-  let productoElegido = prompt("Por favor escriba el nombre de la pintura que le gustaría comprar.");
+  let productoElegido = prompt("Por favor escriba el nombre del producto que le gustaría comprar.");
 
   if (productoElegido === null) {
     alert("La compra ha sido cancelada.");
@@ -90,7 +102,11 @@ function elegirProducto() {
 
      if (productoExiste){
 
-      let cantidadCompra = parseInt (prompt("¿Qué cantidad de producto desea comprar?"));
+      if (productoExiste instanceof Remera) {
+        prompt ("Qué talle de remera quiere comprar?");
+      }
+
+      let cantidadCompra = parseInt (prompt("¿Qué cantidad de productos desea comprar?"));
 
       // Asegurarse de que la cantidad sea válida y que se actualice el stock y el subtotal
 
@@ -99,10 +115,11 @@ function elegirProducto() {
       } else if (cantidadCompra <= productoExiste.cantidad){
         productoExiste.cantidad -= cantidadCompra;
         productoExiste.calcularSubtotal();
+        productoExiste.cantidadCompra = cantidadCompra;
         carrito.push(productoExiste);
     alert("Se agregó el producto " + productoExiste.nombre + " al carrito.");
 
-    const continuar = prompt ("¿Desea seguir agregando producto? Por favor escriba si o no y haga click en aceptar."). toLowerCase();
+    const continuar = prompt ("¿Desea seguir agregando productos? Por favor escriba si o no y haga click en aceptar."). toLowerCase();
     if (continuar === "no"){
       alert ("Procederemos a cerrar su compra.");
       break;
@@ -125,7 +142,7 @@ function mostrarCarrito () {
    
   let mensajeCarrito = "Este es el detalle de su carrito de compras:\n";
   
-   carrito.forEach ((producto) => {mensajeCarrito+= ("- Producto: " + producto.nombre + "\n- Cantidad: " + producto.cantidad + "\n- Subtotal: " + producto.subtotal + "\n\n")});
+   carrito.forEach ((producto) => {mensajeCarrito+= ("- Productos: " + producto.nombre + "\n- Cantidad: " + producto.cantidadCompra + "\n- Subtotal: " + producto.subtotal + "\n\n")});
   
    const total = carrito.reduce ((suma, producto) => suma + producto.subtotal, 0);
    mensajeCarrito += "- Total: " + total;
@@ -135,7 +152,7 @@ function mostrarCarrito () {
   } 
 
    
-  elegirProducto();
+  elegirProductos();
     
 
    localStorage.setItem ("productos",JSON.stringify(stockProductos));
@@ -143,6 +160,9 @@ function mostrarCarrito () {
    localStorage.setItem ("carrito", JSON.stringify(carrito));
 
    localStorage.removeItem("pintura");
+   localStorage.removeItem("Productoss");
+   localStorage.removeItem("Pinturass");
+   localStorage.removeItem("Productos");
 
     
 
