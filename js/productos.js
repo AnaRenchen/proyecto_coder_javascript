b4897175f8c1cb88442d836f6dd021b4bcf10493
AnaRenchen <anamagbh@gmail.com>
2023-12-14 19:55:07 -0300

    gitignore

--- js/productos.js
+++ js/productos.js
@@ -1,245 +1,243 @@
- const productosPinturas = [
-	{
-		id: 1,
-		nombre: "kiku 菊 ",
-		precio: 200,
-		img: 'crisantemos.jpg',
-		stock: 1,
-		descripcion: "Original Painting A3 Size"
-	},
-    {
-        id: 2,
-		nombre: "Dakki no Ohyaku 妲己のお百 ",
-		precio: 380,
-		img: 'calavera.jpg',
-		stock: 1,
-		descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 3,
-        nombre: "Mongaku Shōnin 文覚",
-        precio: 108,
-        img: 'campana.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 4,
-        nombre: "Kiyo Hime 清姫",
-        precio: 450,
-        img: 'campana_grande.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 5,
-        nombre: "Unryu 雲龍",
-        precio: 280,
-        img: 'dragon.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 6,
-        nombre: "Hou-oo 鳳凰",
-        precio: 350,
-        img: 'fenix.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 7,
-        nombre: "Kasha 火車",
-        precio: 190,
-        img: 'gato.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 8,
-        nombre: "Niō (仁王) guardians",
-        precio: 299,
-        img: 'guardianes.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 9,
-        nombre: "Jiraya 自来也",
-        precio: 300,
-        img: 'jiraya.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 10,
-        nombre: "Karajishi 唐獅子",
-        precio: 230,
-        img: 'karajishi.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 11,
-        nombre: "Kingyo 金魚",
-        precio: 290,
-        img: 'kingyo.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 12,
-        nombre: "Yamauba to Kintaro 山姥と金太郎",
-        precio: 400,
-        img: 'kintaro.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 13,
-        nombre: "Kintaro to Koi 金太郎と鯉",
-        precio: 450,
-        img: 'kintaro_koi.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 14,
-        nombre: "Kitsune 狐",
-        precio: 250,
-        img: 'kitsune.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 15,
-        nombre: "Kiyohime 清姫",
-        precio: 380,
-        img: 'mascara_pelos.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 16,
-        nombre: "Orochimaru 大蛇丸",
-        precio: 300,
-        img: 'orochimaru.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 17,
-        nombre: "Tsuru 鶴",
-        precio: 200,
-        img: 'pajaros.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 18,
-        nombre: "Botan 牡丹",
-        precio: 250,
-        img: 'peonias.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 19,
-        nombre: "Danshichi Kurobei 団七九郎兵衛",
-        precio: 500,
-        img: 'samurai_agua.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 20,
-        nombre: "Suimon Yaburi 水門破り",
-        precio: 450,
-        img: 'samurai_espada.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 21,
-        nombre: "Oniwakamaru 鬼若丸",
-        precio: 500,
-        img: 'samurai_koi.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 22,
-        nombre: "Tsunade 綱手",
-        precio: 300,
-        img: 'sunade.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 23,
-        nombre: "Tennin 天人",
-        precio: 300,
-        img: 'tambor.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 24,
-        nombre: "Tora 虎",
-        precio: 300,
-        img: 'tigre.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    },
-    {
-        id: 25,
-        nombre: "Flash",
-        precio: 270,
-        img: 'variado.jpg',
-        stock: 1,
-        descripcion: "Original Painting A3 Size"
-    }
-]
+ const productosPinturas = [{
+         id: 1,
+         nombre: "kiku 菊 ",
+         precio: 200,
+         img: 'crisantemos.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 2,
+         nombre: "Dakki no Ohyaku 妲己のお百 ",
+         precio: 380,
+         img: 'calavera.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 3,
+         nombre: "Mongaku Shōnin 文覚",
+         precio: 108,
+         img: 'campana.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 4,
+         nombre: "Kiyo Hime 清姫",
+         precio: 450,
+         img: 'campana_grande.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 5,
+         nombre: "Unryu 雲龍",
+         precio: 280,
+         img: 'dragon.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 6,
+         nombre: "Hou-oo 鳳凰",
+         precio: 350,
+         img: 'fenix.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 7,
+         nombre: "Kasha 火車",
+         precio: 190,
+         img: 'gato.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 8,
+         nombre: "Niō (仁王) guardians",
+         precio: 299,
+         img: 'guardianes.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 9,
+         nombre: "Jiraya 自来也",
+         precio: 300,
+         img: 'jiraya.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 10,
+         nombre: "Karajishi 唐獅子",
+         precio: 230,
+         img: 'karajishi.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 11,
+         nombre: "Kingyo 金魚",
+         precio: 290,
+         img: 'kingyo.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 12,
+         nombre: "Yamauba to Kintaro 山姥と金太郎",
+         precio: 400,
+         img: 'kintaro.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 13,
+         nombre: "Kintaro to Koi 金太郎と鯉",
+         precio: 450,
+         img: 'kintaro_koi.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 14,
+         nombre: "Kitsune 狐",
+         precio: 250,
+         img: 'kitsune.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 15,
+         nombre: "Kiyohime 清姫",
+         precio: 380,
+         img: 'mascara_pelos.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 16,
+         nombre: "Orochimaru 大蛇丸",
+         precio: 300,
+         img: 'orochimaru.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 17,
+         nombre: "Tsuru 鶴",
+         precio: 200,
+         img: 'pajaros.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 18,
+         nombre: "Botan 牡丹",
+         precio: 250,
+         img: 'peonias.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 19,
+         nombre: "Danshichi Kurobei 団七九郎兵衛",
+         precio: 500,
+         img: 'samurai_agua.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 20,
+         nombre: "Suimon Yaburi 水門破り",
+         precio: 450,
+         img: 'samurai_espada.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 21,
+         nombre: "Oniwakamaru 鬼若丸",
+         precio: 500,
+         img: 'samurai_koi.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 22,
+         nombre: "Tsunade 綱手",
+         precio: 300,
+         img: 'sunade.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 23,
+         nombre: "Tennin 天人",
+         precio: 300,
+         img: 'tambor.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 24,
+         nombre: "Tora 虎",
+         precio: 300,
+         img: 'tigre.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     },
+     {
+         id: 25,
+         nombre: "Flash",
+         precio: 270,
+         img: 'variado.jpg',
+         stock: 1,
+         descripcion: "Original Painting A3 Size"
+     }
+ ]
 
-const productosSouvenirs = [
-    {
-        id: 26,
-        nombre: "Mate y Bombilla",
-        precio: 10,
-        img: 'mate.jpg',
-        stock: 20,
-        descripcion: "+ bombilla y sticker"
-    },
-    {
-        id: 27,
-        nombre: "Pulsera Nenju",
-        precio: 10,
-        img: 'pulsera.jpg',
-        stock: 20,
-        descripcion: "+ sticker y postcard"
-    },
-    {
-        id: 28,
-        nombre: "Remera Fudoshin",
-        precio: 20,
-        img: 'remera_fudoshin.jpeg',
-        stock: 10,
-        descripcion: "Talle Único"
-    },
-    {
-        id: 29,
-        nombre: "Remera Trailokyavijaya",
-        precio: 20,
-        img: 'remera_trai.jpeg',
-        stock: 10,
-        descripcion: "Talle Único"
-    },
-    {
-        id: 30,
-        nombre: "Incienso en polvo",
-        precio: 280,
-        img: 'incienso_polvo.jpeg',
-        stock: 2,
-        descripcion: "para frotar en las manos"
-    }
-]
+ const productosSouvenirs = [{
+         id: 26,
+         nombre: "Mate y Bombilla",
+         precio: 10,
+         img: 'mate.jpg',
+         stock: 20,
+         descripcion: "+ bombilla y sticker"
+     },
+     {
+         id: 27,
+         nombre: "Pulsera Nenju",
+         precio: 10,
+         img: 'pulsera.jpg',
+         stock: 20,
+         descripcion: "+ sticker y postcard"
+     },
+     {
+         id: 28,
+         nombre: "Remera Fudoshin",
+         precio: 20,
+         img: 'remera_fudoshin.jpeg',
+         stock: 10,
+         descripcion: "Talle Único"
+     },
+     {
+         id: 29,
+         nombre: "Remera Trailokyavijaya",
+         precio: 20,
+         img: 'remera_trai.jpeg',
+         stock: 10,
+         descripcion: "Talle Único"
+     },
+     {
+         id: 30,
+         nombre: "Incienso en polvo",
+         precio: 280,
+         img: 'incienso_polvo.jpeg',
+         stock: 2,
+         descripcion: "para frotar en las manos"
+     }
+ ]
